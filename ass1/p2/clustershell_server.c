b6bc44d736a4cd90baf8c3bc38d440a1b174f710
Pranav <thepranavgupta@gmail.com>
2021-03-25 15:17:58 +0530

    cluster server init

--- ass1/p2/clustershell_server.c
+++ ass1/p2/clustershell_server.c
@@ -5,3 +5,49 @@ Clustershell server protocol:
 3. Upon recieving a command from any client, it coordinates the connected machines to execute it
 4. Returns final output to the client
 */
+
+////////////////////////////////////////////
+// Included libraries
+////////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+////////////////////////////////////////////
+// Constants
+////////////////////////////////////////////
+
+// maximum number of clients that will probably connect at a time
+#define MAX_NUM_OF_CONNECTED_CLIENTS 10
+
+////////////////////////////////////////////
+// Data Structures
+////////////////////////////////////////////
+
+// Stores information about a connected client
+typedef struct connected_client_node{
+    char* name;
+    
+}CONNECTED_CLIENT_NODE;
+
+// Stores all the connected clients in a list
+typedef struct connected_clients{
+    int num; // number of connected clients
+    CONNECTED_CLIENT_NODE list[MAX_NUM_OF_CONNECTED_CLIENTS];
+}CONNECTED_CLIENT;
+
+////////////////////////////////////////////
+// Functions
+////////////////////////////////////////////
+
+int main(){
+
+
+
+    return 0;
+}
